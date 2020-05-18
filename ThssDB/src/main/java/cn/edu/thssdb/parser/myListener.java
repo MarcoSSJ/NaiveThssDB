4813ac26312d221c3a39efd3b2fc9184cef6fafd
MarcoSSJ <sunsj16@mails.tsinghua.edu.cn>
2020-05-19 00:45:48 +0800

    建表的处理，需要接口补充

--- ThssDB/src/main/java/cn/edu/thssdb/parser/myListener.java
+++ ThssDB/src/main/java/cn/edu/thssdb/parser/myListener.java
@@ -1,8 +1,14 @@
 package cn.edu.thssdb.parser;
 
+import cn.edu.thssdb.schema.Column;
+import cn.edu.thssdb.schema.Database;
 import cn.edu.thssdb.schema.Manager;
+import cn.edu.thssdb.type.ColumnType;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class myListener extends SQLBaseListener{
     private Manager manager;
@@ -45,6 +51,94 @@ public class myListener extends SQLBaseListener{
         String tableName = ctx.table_name().getText();
         //删除表
         manager.database.drop(tableName);
+    }
+
+    @Override
+    public void exitCreate_table_stmt(SQLParser.Create_table_stmtContext ctx) {
+        String tableName = ctx.table_name().getText();
+        List<SQLParser.Column_defContext> columnDefCtxs = ctx.column_def();
+        int numOfColumns = columnDefCtxs.size();
+        Column[] columns = new Column[numOfColumns];
+        String primary = "";
+        //先处理主键问题
+        List<SQLParser.Column_nameContext> table_constraintContexts = ctx.table_constraint().column_name();
+        if(!table_constraintContexts.isEmpty())
+        {
+//            List<SQLParser.Column_nameContext> column_nameContexts = ctx.table_constraint().column_name();
+//            ArrayList<String> primaryNames = new ArrayList<>();
+//            //int numOfPrimary = column_nameContexts.size();
+//            for (SQLParser.Column_nameContext column_nameContext : column_nameContexts)
+//            {
+//                primaryNames.add(column_nameContext.getText());
+//            }
+//            System.out.println(Arrays.toString(primaryNames.toArray()));
+//            for (int i = 0; i < numOfColumns; i++)
+//            {
+//                //System.out.println(columns[i].name());
+//                if (primaryNames.contains(columns[i].name()))
+//                {
+//                    columns[i].setPrimary(1);
+//                    break;
+//                }
+//            }
+            //主键只有一列
+            List<SQLParser.Column_nameContext> column_nameContexts = ctx.table_constraint().column_name();
+            primary = column_nameContexts.get(0).getText();
+        }
+        for(int i=0;i<numOfColumns;i++)
+        {
+            SQLParser.Column_defContext column_defContext = columnDefCtxs.get(i);
+            String columnName = column_defContext.column_name().getText();
+            String typeRaw = column_defContext.type_name().getText();
+            //预处理type
+            typeRaw = typeRaw.toUpperCase();
+            typeRaw.replaceAll(" ","");
+            StringBuilder typeLength = new StringBuilder("32");
+            if(typeRaw.charAt(0)=='S')
+            {
+                typeLength = new StringBuilder();
+                //识别预设字符串长度
+                for(int j=7;j<typeRaw.length()-1;j++)
+                {
+                    typeLength.append(typeRaw.charAt(j));
+                }
+                typeRaw = "STRING";
+            }
+            int maxLength = Integer.parseInt(typeLength.toString());
+            ColumnType columnType = ColumnType.valueOf(typeRaw);
+            boolean notNull = false;
+            List<SQLParser.Column_constraintContext> column_constraintContexts = column_defContext.column_constraint();
+            if(!column_constraintContexts.isEmpty())
+            {
+                String columnConstraint = column_constraintContexts.get(0).getText();
+                //not null only
+                if (columnConstraint.toUpperCase().equals("NOTNULL"))
+                {
+                    notNull = true;
+                }
+            }
+            if(columnName.equals(primary))
+                columns[i] = new Column(columnName,columnType,1, notNull, maxLength);
+            else
+                columns[i] = new Column(columnName,columnType,0, notNull, maxLength);
+        }
+
+//        try {
+//            Database db = manager.getCurrentDB();
+//            if(db.containsTable(tableName)){
+//                success = false;
+//                status.msg += "Duplicated tableName.\n";
+//            }
+//            else {
+//                db.create(tableName, columns);
+//                status.msg += "Create table successfully.\n";
+//            }
+//        }catch (Exception e){
+//            success = false;
+//            status.msg+="Failed to create table.";
+//        }
+        //TODO:
+        //建立表的接口
     }
 
     @Override
